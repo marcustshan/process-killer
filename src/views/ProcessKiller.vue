33af4423cf11b019cea6bd1614a2b6d5997b4286
tshan <tshan@byulsoft.com>
2020-04-20 18:26:53 +0900

    jps 기능 추가 (ing)

--- src/views/ProcessKiller.vue
+++ src/views/ProcessKiller.vue
@@ -21,10 +21,11 @@
             </li>
             <li class="item-name">
               {{ task.imageName }}
+              <button v-if="task.javaTask && task.javaTask.pid" @click="fnShowJavaDetail(task.javaTask)" class="btn detail">상세보기</button>
             </li>
             <li class="item-port">
               {{ task.local.port }}
-              <button @click="fnKillProcess(task.pid)" class="btn-kill">&times;</button>
+              <button @click="fnKillProcess(task.pid)" class="btn kill">&times;</button>
             </li>
           </ul>
         </li>
@@ -43,10 +44,46 @@ export default {
       search: '',
       filteredTasks: [],
       tasks: [],
-      nets: []
+      nets: [],
+      javaTasks: []
     }
   },
   methods: {
+    fnShowJavaDetail (javaTask) {
+      console.log(javaTask)
+    },
+    fnCmdJpsDataCallback (process) {
+      process = process.split('  ').join(' ')
+      const processArgs = process.split(' ')
+      const javaTask = {
+        pid: processArgs[0],
+        name: processArgs[1],
+        args: ''
+      }
+      delete processArgs[0]
+      delete processArgs[1]
+      javaTask.args = processArgs.join(' ')
+      javaTask.args = javaTask.args.substring(2)
+
+      this.javaTasks.push(javaTask)
+    },
+    fnCmdJpsDone () {
+      this.javaTasks.forEach(javaTask => {
+        this.tasks.some(task => {
+          if (Number(task.pid) === Number(javaTask.pid)) {
+            task.javaTask = javaTask
+            return task.pid === javaTask.pid
+          }
+        })
+      })
+    },
+    fnCmdJps () {
+      const nrc = require('node-run-cmd')
+      nrc.run('jps -v', {
+        onData: this.fnCmdJpsDataCallback,
+        onDone: this.fnCmdJpsDone
+      })
+    },
     fnCloneDeep (target) {
       return JSON.parse(JSON.stringify(target))
     },
@@ -101,6 +138,8 @@ export default {
 
       this.tasks = _.uniqWith(this.tasks, _.isEqual)
       this.filteredTasks = this.tasks
+
+      this.fnCmdJps()
     }
   },
   mounted () {
@@ -122,5 +161,7 @@ export default {
   ul.list li.item-pid {width: 30%;}
   ul.list li.item-name {width: 40%;}
   ul.list li.item-port {width: 30%;}
-  .btn-kill {position: absolute; width: 20px; height: 20px; top: 5px; right: 5px; cursor: pointer; border: 1px solid #da0e0e; background-color: #da0e0e; color: #f1f1f1; font-weight: 600;}
+  .btn {position: absolute; width: 20px; height: 20px; top: 5px; right: 5px; cursor: pointer;}
+  .btn.kill {border: 1px solid #da0e0e; background-color: #da0e0e; color: #f1f1f1; font-weight: 600;}
+  .btn.detail {border: 1px solid #0eda85; background-color: #0eda85; color: #777; font-weight: 600;}
 </style>
